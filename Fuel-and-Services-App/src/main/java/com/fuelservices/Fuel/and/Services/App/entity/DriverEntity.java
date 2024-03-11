33097041b0bcf6dbb90755ecd8073d35bfd4344c
mubarak <aloraifan505@hotmail.com>
2024-03-11 23:38:38 +0300

    Adding some Edits

--- Fuel-and-Services-App/src/main/java/com/fuelservices/Fuel/and/Services/App/entity/DriverEntity.java
+++ Fuel-and-Services-App/src/main/java/com/fuelservices/Fuel/and/Services/App/entity/DriverEntity.java
@@ -11,4 +11,23 @@ public class DriverEntity {
     @Column(name = "driver_Id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long driver_Id;
+
+    @Column(name = "plate_license",nullable = false)
+    private String plate_license;
+
+    public Long getDriver_Id() {
+        return driver_Id;
+    }
+
+    public void setDriver_Id(Long driver_Id) {
+        this.driver_Id = driver_Id;
+    }
+
+    public String getPlate_license() {
+        return plate_license;
+    }
+
+    public void setPlate_license(String plate_license) {
+        this.plate_license = plate_license;
+    }
 }
--- Fuel-and-Services-App/src/main/java/com/fuelservices/Fuel/and/Services/App/entity/RequestEntity.java
+++ Fuel-and-Services-App/src/main/java/com/fuelservices/Fuel/and/Services/App/entity/RequestEntity.java
@@ -14,13 +14,14 @@ public class RequestEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long request_Id;
 
+    @Column(name = "location",nullable = false)
+    private String location;
     @Enumerated(EnumType.STRING)
 private CarType carType;
     @Enumerated(EnumType.STRING)
 private RequestType requestType;
 
-    @Column(name = "",nullable = false)
-    private String location;
+
 
     @OneToOne
     @JoinColumn(name = "service_Id")
@@ -33,4 +34,60 @@ private RequestType requestType;
     @OneToOne
     @JoinColumn(name = "driver_Id")
     private DriverEntity driverEntity;
+
+    public Long getRequest_Id() {
+        return request_Id;
+    }
+
+    public void setRequest_Id(Long request_Id) {
+        this.request_Id = request_Id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+
+    public RequestType getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(RequestType requestType) {
+        this.requestType = requestType;
+    }
+
+    public ServiceEntity getServiceEntity() {
+        return serviceEntity;
+    }
+
+    public void setServiceEntity(ServiceEntity serviceEntity) {
+        this.serviceEntity = serviceEntity;
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    public DriverEntity getDriverEntity() {
+        return driverEntity;
+    }
+
+    public void setDriverEntity(DriverEntity driverEntity) {
+        this.driverEntity = driverEntity;
+    }
 }
